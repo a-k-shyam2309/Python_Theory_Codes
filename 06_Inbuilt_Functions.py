5e347dad89c951e893688feecd570adea97ea1fd
Aditya Kumar Shyam <a.k.shyam2309@gmail.com>
2025-12-01 00:31:38 +0530

    Update and rename 06.py to 06_Inbuilt_Functions.py

--- 06_Inbuilt_Functions.py
+++ 06_Inbuilt_Functions.py
@@ -8,16 +8,28 @@
 # d[2][0] = 100
 # print(id(a[2][0]) == id(d[2][0]))
 
+
 # A = [15 , 17 , 3 , 9 , 7]
 # print(len(A))
 # print(max(A))
 # print(min(A))
-# print(sum(A))
+# print(sum(A))M
 # print(sum(A)/len(A))
 # print(sorted(A))
 # print(A)
+
+# a = [1,3,5,0]
+# for i in reversed(a):
+#     print(i)
+#O/P:-
+# 0 5 3 1
+
+
 # str = "python"
 # print(sorted(str))
+#O/P:-
+#['h', 'n', 'o', 'p', 't', 'y']
+
 
 # a = (1,3,5,0)
 # for i in reversed(a):
@@ -26,9 +38,13 @@
 # l = [1,5,3,7]
 # print(tuple(reversed(l)))
 
+
+
 # names= ["Ravi" , "Radha" , "Aman" , "Meera"]
 # marks = [80 , 75 , 91 , 87]
 # print(list(zip(names,marks)))
+#O/P:-
+# [('Ravi', 80), ('Radha', 75), ('Aman', 91), ('Meera', 87)]
 
 # marks.append([10,23,34])
 # print(marks)
@@ -36,7 +52,7 @@
 # print(marks)
 
 # B = [23,27,76,65,1,90]
-# B.sort(reverse = True)
+# B.sort(reverse = True)            ==> SORT FUNCTION
 # print(B)
 
 # fruits = ["apple" , "banana" , "kiwi" , "orange" , "kiwo"]
@@ -56,22 +72,23 @@
 # print(a)
 
 
-list1 = []
-for i in range(1,101):
-    list1.append(i)
+# list1 = []
+# for i in range(1,101):
+#     list1.append(i)
 
-list2 =[]
-for x in list1:
-    count=0
-    if x==1:
-        continue
-    for y in range(2,11):
-        if x==y:
-            continue
-        if x%y==0:
-            count+=1
+# list2 =[]
+# for x in list1:
+#     count=0
+#     if x==1:
+#         continue
+#     for y in range(2,11):
+#         if x==y:
+#             continue
+#         if x%y==0:
+#             count+=1
 
-    if count==0:
-        list2.append(x)
+#     if count==0:
+#         list2.append(x)
         
-print(list2)
+# print(list2)
+
