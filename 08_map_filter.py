100b95de0c1b34d9c256e672c8f7581f5653b6db
Aditya Kumar Shyam <a.k.shyam2309@gmail.com>
2025-12-02 00:19:13 +0530

    Update 08_map_filter.py

--- 08_map_filter.py
+++ 08_map_filter.py
@@ -1,18 +1,6 @@
-# from functools import reduce
-# list1 = [1,2,3,4,5]
-# print(reduce(lambda x,y : x if (x>y) else y ,list1))
 
-
-
-# from functools import reduce
-# num = 1234567
-# snum = str(num)
-# print(reduce(lambda x,y : int(x) + int(y) ,snum))
-
-
-# from functools import reduce
-# print(reduce(lambda x,y : x*y ,range(1,6))) 
 
 # from functools import reduce
 # num = [1,2,7,4,3,5,2,6,3]
 # print( reduce(lambda x,y : x+y ,list(map(lambda x : x*x ,list(filter(lambda x : x%2==0 , num))))))
+
