80a361125b1a91f39b177cdc4d81366f613dfdde
Aditya Kumar Shyam <a.k.shyam2309@gmail.com>
2025-12-02 00:45:35 +0530

    Update and rename 08_map_filter.py to 08_ListComprehension_map_filter.py

---
+++ 08_ListComprehension_map_filter.py
@@ -0,0 +1,54 @@
+# LIST COMPREHENSION #
+
+# list1 = [1,2,3,4,5]
+# list2 = list1[:]		# using slicing
+# list3 = []
+# for i in list1:			# using loop method
+#     list3.append(i)
+# list4 = [j for j in list1]		# using list comprehension
+# print(list1)
+# print(list2)
+# print(list3)
+# print(list4)
+
+
+# lst1 = [2,4,6,8,10]
+# lst2 = [i**2 for i in lst1]
+# print(lst2)
+
+
+# a = [i+10 for i in range(10)]
+# print(a)
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# IF CONDITION IN LIST COMPREHENSION :-
+# list1 = [1,2,3,4,5,6,7,8]
+# list2 = [j*j for j in list1 if (j%2==0)]  # SQUARING THE EVEN NOS
+# print(list2)
+
+
+
+# IF-ELSE CONDITION IN LIST COMPREHENSION :-
+# list1 = [1,2,3,4,5,6,7,8]
+# list2 = ["Even" if (j%2==0) else "odd" for j in list1]   	
+# print(list2)
+
+
+
+# IF - ELSE-IF CONDITION IN LIST COMPREHENSION :-
+# list1 = [1,2,3,4,5,6,7,8]
+# list2 = ["Small" if (j<3) else "Mid" if (j<6) else "Large" for j in list1]   	
+# print(list2)
+
+
+
+
+
+
+
+# from functools import reduce
+# num = [1,2,7,4,3,5,2,6,3]
+# print( reduce(lambda x,y : x+y ,list(map(lambda x : x*x ,list(filter(lambda x : x%2==0 , num))))))
+
+
--- 08_map_filter.py
+++
@@ -1,6 +0,0 @@
-
-
-# from functools import reduce
-# num = [1,2,7,4,3,5,2,6,3]
-# print( reduce(lambda x,y : x+y ,list(map(lambda x : x*x ,list(filter(lambda x : x%2==0 , num))))))
-
