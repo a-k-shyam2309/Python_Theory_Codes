4f689ca71607f61d589e8dc6076900be1885fd0a
Aditya Kumar Shyam <a.k.shyam2309@gmail.com>
2025-12-01 12:16:24 +0530

    Update 07.py

--- Assignment-4/07.py
+++ Assignment-4/07.py
@@ -1,1 +1,13 @@
 
+#  * --> Anytimes
+#  . --> Any Characters
+
+import re
+text = "Random <tag> First </tag> some text <tag> second </tag> end"
+pat = r'<tag>.*</tag>'
+m = re.findall(pat , text)
+
+print(m)
+
+for i in m:
+    print(i)
