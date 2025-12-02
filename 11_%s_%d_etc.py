1cba9335ba811cea1325554eb18c8376db7857ce
Aditya Kumar Shyam <a.k.shyam2309@gmail.com>
2025-12-03 01:07:38 +0530

    Rename 11.py to 11_%s_%d_etc.py

---
+++ 11_%s_%d_etc.py
@@ -0,0 +1,45 @@
+# %d --> Integer / Number
+# %s --> String
+# %f --> Float
+# %r --> keeps the string in ' ' (see line 17 & 18)
+
+
+# a=75
+# b=25
+# c=100
+# print("%d plus %d equals %d" %(a,b,c))      O/P --> 75 plus 25 equals 100
+
+# str = "Anmol"
+# print("My name is %s" %str)                 O/P --> My name is Anmol
+
+# pi = 3.1426
+# print("The value of %f" %pi)                O/P --> The value of 3.142600
+
+# str = "Hello"
+# print("Say to him %r" %str)                 O/P --> Say to him 'Hello'
+
+
+
+# n1="e9"
+# n2="af"  
+
+# d1 = int(n1 , 16)
+# d2 = int(n2 , 16)
+# print("The result in hex is %x" %(d1+d2))     O/P --> The result in hex is 198
+
+
+
+# print("Amount is |%d|" %25)
+# print("Amount is |%10d|" %25)
+# print("Amount is |%.5d|" %25)
+# print("Amount is | %10.5d|" %25)
+
+# O/P :-
+# Amount is |25|
+# Amount is |        25|
+# Amount is |00025|
+# Amount is |     00025|
+
+
+# print("(%08.2f)" %12.346)     o/p --> (00012.35)
+
