c7279a3a51fb96b9f6b4cc5bd1592dfa59c1f090
Aditya Kumar Shyam <a.k.shyam2309@gmail.com>
2025-12-03 00:44:06 +0530

    Rename file to 08_ListComprehension_filter.py

--- 08_ListComprehension_filter.py
+++ 08_ListComprehension_filter.py
@@ -44,34 +44,6 @@
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
-# map():-
-
-
-# -> it resolves the iterables value one by one and perform operations according to the function.
-# syntax :- map(fun , iterable)
-# lazy function so we will use list()
-
-
-# map only
-# nums = [1,2,3,4,5]
-# def add(a):
-#     return a+100
-# squares = list(map(add , nums))				# map
-# print(squares)
-
-
-# lambda + map
-# nums = [1,2,3,4,5]
-# squares = list(map(lambda x: x*x , nums))		# lambda & map
-# print(squares)
-
-
-# List comprehension + lambda + map
-# a = [i+10 for i in range(10)]					# List Comprehension
-# squares = list(map(lambda x: x*x , a))  		# lambda & map
-# print(squares)
-
-
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
@@ -114,6 +86,7 @@
 # from functools import reduce
 # num = [1,2,7,4,3,5,2,6,3]
 # print( reduce(lambda x,y : x+y ,list(map(lambda x : x*x ,list(filter(lambda x : x%2==0 , num))))))
+
 
 
 
